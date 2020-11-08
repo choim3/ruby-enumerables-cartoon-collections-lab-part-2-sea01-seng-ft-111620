7fd225db4eaa5aaf2c85bb6cbcb44397df3a522b
Matt Choi <choim3@spu.edu>
2020-11-08 21:48:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,7 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   
- v = planeteer_calls.find do |call_check| 
-    call_check.include?(valid_calls)
+    planeteer_calls.find do |call_check| 
+    valid_calls.include?()
   end
-  puts v
 end
